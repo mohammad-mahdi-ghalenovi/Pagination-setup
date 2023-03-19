72fbb5e9181c8ac8208e50111a0aeafc82dc2b1f
Metti <107748176+Mettiuy@users.noreply.github.com>
2023-03-19 23:41:08 +0330

    array created

---
+++ app.js
@@ -0,0 +1,18 @@
+let names = [
+  "metti",
+  "ali",
+  "akbar",
+  "soghra",
+  "khodet",
+  "amat",
+  "babat",
+  "amat",
+  "hossein",
+  "nmishnasam",
+  "1",
+  "2",
+  "3",
+  "4",
+];
+let menuContainer = document.querySelector(".menu-container");
+let btnContainer = document.querySelector(".btn-container");
