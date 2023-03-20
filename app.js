f1f965cfad298addb0becc3d9f9f6c64aaaf63f1
Metti <107748176+Mettiuy@users.noreply.github.com>
2023-03-20 10:41:07 +0330

    some changes were made

--- app.js
+++ app.js
@@ -2,23 +2,27 @@ let names = [
   "metti",
   "ali",
   "akbar",
+
   "soghra",
   "khodet",
   "amat",
+
   "babat",
   "amat",
   "hossein",
+
   "nmishnasam",
   "1",
   "2",
+
   "3",
   "4",
 ];
 let menuContainer = document.querySelector(".menu-container");
 let btnContainer = document.querySelector(".btn-container");
 
-let rowCount = 3;
-let currentPage = 1;
+let rowCount = 3;        // you can change count of rows 
+let currentPage = 1;   
 let startIndex;
 let endIndex;
 
